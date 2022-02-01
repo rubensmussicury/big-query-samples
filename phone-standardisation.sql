8f2416836d069ee45d12a87da0b3bc6d079ecce2
rubensmussicury <rubensmussicury@users.noreply.github.com>
2022-02-01 22:54:50 +0000

    Update phone-standardisation.sql

--- phone-standardisation.sql
+++ phone-standardisation.sql
@@ -2,31 +2,33 @@
  FILE         : phone-standardisation.sql
  NAME         : Phone Standardisation on BigQuery
  DESCRIPTION  : Sample of how standardise phone number using external JS
-                library no BigQuery
+                library on BigQuery.
  AUTHOR       : Rubens Mussi Cury
  DATE         : 01-02-2022
 =================================================================================
- CHANGE HISTORY
-=================================================================================
-PR  DATE        AUTHOR         DESCRIPTION 
---  ----------  -------------  --------------------------------------------------
-
-*********************************************************************************/
 
 DECLARE PHONE_NUMS ARRAY<STRING>;
 
 # All phones were found in a public websites.
-# Used in this simulation the string ":" to add the country code - when needed.
-SET PHONE_NUMS = ["+61261960196", "+1 202-501-4444", "+12020121234:US",
-                  "+55 61 3311 6197", "1199321-1251:BR", "+44 191 203 7010",
-                  "4407958013984:GB", "+330484826377", "00010101010"];
+# Used in this simulation string ":" to add the country code - when needed.
+SET PHONE_NUMS = ["+61261960196", 
+                  "+1 202-501-4444", 
+                  "+12020121234:US", 
+                   "+55 61 3311 6197",
+                   "1199321-1251:BR",
+                   "+44 191 203 7010",
+                   "4407958013984:GB",
+                   "+330484826377",
+                   "00010101010"];
                   
 CREATE TEMP FUNCTION STD_PHONE(phoneString STRING)
   RETURNS STRING
   LANGUAGE js
   # Download the compiled JS and upload to your GCS
   # https://catamphetamine.gitlab.io/libphonenumber-js/libphonenumber-max.js
-  OPTIONS (library=["gs://your-bucket/libphonenumber-max.js"])
+  # Original - https://github.com/google/libphonenumber
+  # Rewrite - https://catamphetamine.gitlab.io/libphonenumber-js
+  OPTIONS (library=["gs://my-bucket/libphonenumber-max.js"])
 AS
 r"""
 
@@ -40,7 +42,7 @@ try {
         'Return': 'OK',
         'Country': number.country || '—',
         'National': number.formatNational(),
-        'International': number.formatInternational(),
+        'Overseas': number.formatInternational(),
         'Type': number.getType() || '—',
         'Valid': number.isValid()
     }
@@ -55,12 +57,12 @@ return JSON.stringify(result)
 
 SELECT
   SPLIT(PHONE, ":")[OFFSET(0)] AS TYPED_PHONE,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Return") AS Return,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Country") AS Country,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.National") AS National,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.International") AS International,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Type") AS Type,
-  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Valid") AS Valid
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Return") AS RETURN,
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Country") AS COUNTRY,
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.National") AS NATIONAL,
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Overseas") AS OVERSEAS,
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Type") AS TYPE,
+  JSON_EXTRACT_SCALAR(STD_PHONE(PHONE), "$.Valid") AS VALID
 FROM 
   UNNEST(PHONE_NUMS) AS PHONE
               
